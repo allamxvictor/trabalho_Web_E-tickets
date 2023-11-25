417a940dbe8863ab165b7322f3a591aa404c863c
Allan Victor <allamxvictorx@gmail.com>
2023-11-25 06:29:40 -0300

    useStates adicionados ao form de cadastro, falta implementar o back e bd

--- src/componentes/FormCadastro/index.jsx
+++ src/componentes/FormCadastro/index.jsx
@@ -1,26 +1,42 @@
 import styles from "./FormCadastro.module.css"
-import Button from "../Button"
+import { useState } from "react"
 
 export default function FormCadastro() {
+   
+    function handleCadastro(event){
+        event.preventDefault() 
+
+        console.log(nome, email, dataNascimento, telefone, cpf, senha) // apagar dps.
+
+        //aqui entra a lógica para o back enviar os dados pro banco.
+
+    }
+   
+    const [nome, setNome] = useState()
+    const [email, setEmail] = useState()
+    const [dataNascimento, setDataNascimento] = useState()
+    const [telefone, setTelefone] = useState()
+    const [cpf, setCpf] = useState()
+    const [senha, setSenha] = useState()
+
     return (
         
         <section className={styles.form_container}>
             <h2>Crie sua conta</h2>
 
             
-            <form action="" className={styles.form_inputs}>
-                <input type="text" placeholder="Nome" /> 
-                <input type="email" placeholder="Email" />
-                <input type="text" placeholder="Data de nascimento" />
-                <input type="tel" placeholder="Telefone" />
-                <input type="text" placeholder="CPF" />
-                <input type="text" placeholder="Senha" />
+            <form className={styles.form_inputs}>
+                <input type="text" placeholder="Nome" onChange={(event) => setNome(event.target.value)} /> 
+                <input type="email" placeholder="Email" onChange={(event) => setEmail(event.target.value)}/>
+                <input type="text" placeholder="Data de nascimento" onChange={(event) => setDataNascimento(event.target.value)}/>
+                <input type="tel" placeholder="Telefone" onChange={(event) => setTelefone(event.target.value)}/>
+                <input type="text" placeholder="CPF" onChange={(event) => setCpf(event.target.value)}/>
+                <input type="password" placeholder="Senha" onChange={(event) => setSenha(event.target.value)}/>
+
+                <button type="submit" onClick={handleCadastro}>Cadastrar</button>
+
             </form>
-
-            <Button button_txt="Cadastrar" />
             
-
-
         </section>
     )
 }
--- src/componentes/FormLogin/index.jsx
+++ src/componentes/FormLogin/index.jsx
@@ -4,11 +4,11 @@ import styles from "./FormLogin.module.css"
 export default function FormLogin() {
     
     const [user, setUser] = useState()
-    const [password, setPassword] = useState()
+    const [senha, setSenha] = useState()
 
     function handleLogin(event){
         event.preventDefault()
-        console.log(user, password) //está aqui apenas se quiser a se os dados chegaram a função. Apagar apos implementar a lógica de banco 
+        console.log(user, senha) //está aqui apenas se quiser a se os dados chegaram a função. Apagar apos implementar a lógica de banco 
 
         // aqui entra a lógica de verificação no banco de dados..
 
@@ -20,15 +20,12 @@ export default function FormLogin() {
                 <h2>Bem-vindo!</h2>
 
                 <input type="text" placeholder="Email" onChange={(event) => setUser(event.target.value)} />
-                <input type="password" placeholder="Senha" onChange={(event) => setPassword(event.target.value)} />
+                <input type="password" placeholder="Senha" onChange={(event) => setSenha(event.target.value)} />
 
-                <button onClick={handleLogin}>Entrar</button>
+                <button type="submit" onClick={handleLogin}>Entrar</button>
 
             </form>
         </>
-   
+
     )
-
-   
-
 }
